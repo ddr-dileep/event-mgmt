0460da642a72e427835776ec5055a2ffe71cb163
Dileep-Raloti <87886096+Dileep-Raloti@users.noreply.github.com>
2021-08-10 22:10:08 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,9 +7,9 @@ function setTheme(themeName) {
 // function to toggle between light and dark theme
 function toggleTheme() {
     if (localStorage.getItem('theme') === 'theme-dark') {
-        setTheme('theme-dark');
-    } else {
         setTheme('theme-light');
+    } else {
+        setTheme('theme-dark');
     }
 }
 
